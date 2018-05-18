b5627ccfc0d5bbe6acf0d181c27bd30146f6dcfe
ricky-wangprof <ricky.wangprof@gmail.com>
2018-05-17 23:28:48 -0400

    check in test plot for plotly and cufflinks

--- test_plot.py
+++ test_plot.py
@@ -1,7 +1,21 @@
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+import pandas as pd
 import numpy as np
 
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+print(__version__) # requires version >= 1.9.0
+
+import cufflinks as cf
+
+# For Notebooks
+init_notebook_mode(connected=True)
+
+# For offline use
+cf.go_offline()
+
+df = pd.DataFrame(np.random.randn(100,4),columns='A B C D'.split())
+
+df.head()
+
+df.iplot(kind='scatter',x='A',y='B',mode='markers',size=10)
